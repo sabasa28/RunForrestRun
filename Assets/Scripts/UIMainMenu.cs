1ac724c235a3a6309842a3e44543731f53b34679
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2025-02-10 03:56:58 -0300

    Agregado menu

---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject InstructionsPanel;
+
+    public void ShowInstructions(bool bShouldShow)
+    {
+        InstructionsPanel.SetActive(bShouldShow);
+    }
+
+    public void GoToGameplayScene()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("El juego se hubiese cerrado si no estuvieses en editor");
+    }
+}
